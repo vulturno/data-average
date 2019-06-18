be61e9b5a80d0f9eb00364266e6f3014eba1d96e
Jorge Aznar <jorgeatgu@gmail.com>
2019-06-18 11:36:27 +0200

    remove double quotes | first attempt to mean

--- average.sh
+++ average.sh
@@ -1,3 +1,10 @@
 #!/usr/local/bin/bash
 
-jq -c 'map(select(.fecha | contains("-08-")))'
+# Guardamos en un array los días del año
+readarray -t days < ~/github/data-average/data/dias.csv
+
+for ((j = 0; j < ${#days[@]}; j++)); do
+
+    csvgrep -c fecha -r "${days[$j]}$" ~/github/data-average/data/9434-1980-2009.csv | csvstat -c maxima --mean > temp.csv
+
+done
