d8b9b63bf4608534306cc9eb0e0699a5e376142a
Jorge Aznar <jorgeatgu@gmail.com>
2019-06-19 10:03:08 +0200

    mean

--- average.sh
+++ average.sh
@@ -15,7 +15,7 @@ readarray -t days < ~/github/data-average/data/dias.csv
 
 for ((j = 0; j < ${#days[@]}; j++)); do
 
-    csvgrep -c fecha -r "${days[$j]}$" ~/github/data-average/data/9434-2010-2019.csv | csvstat -c maxima --mean >> temp.csv
+    csvgrep -c fecha -r "${days[$j]}$" ~/github/data-average/data/9434-2010-2019.csv | csvstat -c media --mean >> temp.csv
 
 done
 
